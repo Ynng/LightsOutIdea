45cfc2e704d90a209260623a7337249a9e6a1be6
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2018-12-21 13:57:05 -0500

    added a text field for testing

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,12 +5,19 @@ public class MainFrame extends JFrame {
     static private int TILE_SIZE = 100;
     static private int GAME_SIZE = 5;
 
+    private JTextArea text;
+
+
     public MainFrame() {
         super("Lights Out");
         setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
+
+        text = new JTextArea();
+        add(text, BorderLayout.CENTER);
+
         setSize(TILE_SIZE * GAME_SIZE, TILE_SIZE * GAME_SIZE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 }
