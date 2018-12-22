1248e8764d1bd1fa56b39182b30f373089093683
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2018-12-21 20:45:42 -0500

    Added a random with range method for convenience

--- src/LightsOut.java
+++ src/LightsOut.java
@@ -3,15 +3,22 @@ import javax.swing.*;
 public class LightsOut {
     static int TILE_SIZE = 100;
     static int GRID_SIZE = 5;
+    static int TILE_MARGIN = 10;
+    static int TILE_BORDER = 5;
 
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                MainFrame frame = new MainFrame();
+                new MainFrame();
             }
         });
 
+    }
+
+    static int randomRange(int min, int max) {
+        int range = (max - min) + 1;
+        return (int) (Math.random() * range) + min;
     }
 }
