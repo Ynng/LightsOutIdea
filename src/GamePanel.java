82e1cd88e9797b6c8b22d93110b416dcc9c57bc9
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2018-12-21 21:16:57 -0500

    polished the drawing process

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class GamePanel extends JPanel {
+    static int TILE_SIZE_WITH_BORDER = LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER * 2;
+
     public GamePanel() {
         setPreferredSize(
                 new Dimension(LightsOut.TILE_SIZE * LightsOut.GRID_SIZE, LightsOut.TILE_SIZE * LightsOut.GRID_SIZE));
@@ -10,20 +13,32 @@ public class GamePanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g);
         int y = LightsOut.TILE_MARGIN / 2;
         for (int i = 0; i < LightsOut.GRID_SIZE; i++) {
             int x = LightsOut.TILE_MARGIN / 2;
             for (int j = 0; j < LightsOut.GRID_SIZE; j++) {
-                // drawing the border of the tiles
-                g.setColor(Color.GREEN);
-                g.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
-                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
-                // drawing the actuall tiles
-                g.setColor(Color.WHITE);
-                g.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER,
-                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER*2,
-                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER*2);
+                if (LightsOut.HAS_THE_WORLD_ENDED_YET) {
+
+                    // drawing the actuall tiles
+                    g2.setColor(Color.WHITE);
+                    // g2.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER,
+                    // ACTUALL_TILE_SIZE, ACTUALL_TILE_SIZE);
+                    g2.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
+                            LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
+                    // drawing the border of the tiles
+                } else {
+                    // drawing the border
+                    g2.setColor(Color.GREEN);
+                    g2.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
+                            LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
+                    // drawing the actuall tile
+                    g2.setColor(Color.WHITE);
+                    g2.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER, TILE_SIZE_WITH_BORDER,
+                            TILE_SIZE_WITH_BORDER);
+
+                }
 
                 x += LightsOut.TILE_SIZE;
             }
