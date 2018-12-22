2ab6428e2336d8f97434b1af13a9404a61a15762
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2018-12-21 20:44:54 -0500

    changed the way the UI is drawn to be more versatile

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,35 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
-    public GamePanel(int tile, int grid) {
-        GridLayout layout = new GridLayout(grid, grid, tile / 15, tile / 15);
-        setLayout(layout);
-        setBackground(Color.BLUE);
-        // for (int i = 0; i < grid; i++) {
-        // for (int j = 0; j < grid; j++) {
-        // add(new JButton("Button " + i + " " + j));
-        // }
-        // }
-        repaint();
+    public GamePanel() {
+        setPreferredSize(
+                new Dimension(LightsOut.TILE_SIZE * LightsOut.GRID_SIZE, LightsOut.TILE_SIZE * LightsOut.GRID_SIZE));
+        setBackground(Color.PINK);
     }
-
-    
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int y = 0;
+        int y = LightsOut.TILE_MARGIN / 2;
         for (int i = 0; i < LightsOut.GRID_SIZE; i++) {
-            int x = 0;
+            int x = LightsOut.TILE_MARGIN / 2;
             for (int j = 0; j < LightsOut.GRID_SIZE; j++) {
-                g.setColor(Color.WHITE);
-                g.fillRect(x, y, LightsOut.TILE_SIZE, LightsOut.TILE_SIZE);
+                // drawing the border of the tiles
                 g.setColor(Color.GREEN);
-                g.drawRect(x, y, LightsOut.TILE_SIZE, LightsOut.TILE_SIZE);
+                g.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
+                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
+                // drawing the actuall tiles
+                g.setColor(Color.WHITE);
+                g.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER,
+                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER*2,
+                        LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER*2);
 
-                x+=LightsOut.TILE_SIZE;
+                x += LightsOut.TILE_SIZE;
             }
-            y+=LightsOut.TILE_SIZE;
+            y += LightsOut.TILE_SIZE;
         }
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,20 +3,20 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-
     private GamePanel panel;
-
 
     public MainFrame() {
         super("Lights Out");
         setLayout(new BorderLayout());
-        setResizable(false);
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel = new GamePanel(LightsOut.TILE_SIZE, LightsOut.TILE_SIZE);
+
+        panel = new GamePanel();
         add(panel, BorderLayout.CENTER);
 
-        setSize(LightsOut.TILE_SIZE * LightsOut.GRID_SIZE, LightsOut.TILE_SIZE * LightsOut.GRID_SIZE);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setSize(LightsOut.TILE_SIZE * LightsOut.GRID_SIZE, LightsOut.TILE_SIZE * LightsOut.GRID_SIZE);
+        pack();
         setVisible(true);
     }
 }
