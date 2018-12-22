3646b480b522f8e61c2c6ba5039b8a4c61065a95
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2018-12-21 22:09:06 -0500

    implemented level generation and drawing based on actuall game data

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,11 +4,79 @@ import java.awt.geom.Rectangle2D;
 
 public class GamePanel extends JPanel {
     static int TILE_SIZE_WITH_BORDER = LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN - LightsOut.TILE_BORDER * 2;
+    static int TILE_SIZE_WITH_MARGIN = LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN;
+
+    // 2d arrays for storing the light states
+    private boolean[][] lights;
+    private boolean[][] lightsClicked;
+    // stepp counter to record the number of steps the user have taken
+    public int stepCounter = 0;
 
     public GamePanel() {
         setPreferredSize(
                 new Dimension(LightsOut.TILE_SIZE * LightsOut.GRID_SIZE, LightsOut.TILE_SIZE * LightsOut.GRID_SIZE));
         setBackground(Color.PINK);
+
+        // initialize the two arrays
+        lights = new boolean[5][5];
+        lightsClicked = new boolean[5][5];
+
+        // randomly turn on 10 lights
+        while (true) {
+            int x = LightsOut.randomRange(0, 4);
+            int y = LightsOut.randomRange(0, 4);
+            lightsClicked[x][y] = !lightsClicked[x][y];
+            // System.out.println((x + 1) + " " + (y + 1) + "\n");
+            toggle(lights, x, y);
+            if (countBools(lights) == 10)
+                break;
+        }
+        printBools(lightsClicked);
+        System.out.println("It will take atleast: " + countBools(lightsClicked) + " steps to complete");
+
+    }
+
+    public void printBools(boolean[][] input) {
+        for (boolean[] list : input) {
+            for (boolean light : list) {
+                System.out.print((light ? "1" : "0") + " ");
+            }
+            System.out.print('\n');
+        }
+    }
+
+    public int countBools(boolean[][] input) {
+        int count = 0;
+        for (boolean[] list : input) {
+            for (boolean item : list) {
+                if (item) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void toggle(boolean[][] input, int row, int col) {
+
+        input[row][col] = !input[row][col];
+        if (row - 1 >= 0) {
+            input[row - 1][col] = !input[row - 1][col];
+
+        }
+        if (row + 1 <= 4) {
+            input[row + 1][col] = !input[row + 1][col];
+
+        }
+        if (col - 1 >= 0) {
+            input[row][col - 1] = !input[row][col - 1];
+
+        }
+        if (col + 1 <= 4) {
+            input[row][col + 1] = !input[row][col + 1];
+
+        }
+
     }
 
     @Override
@@ -19,27 +87,28 @@ public class GamePanel extends JPanel {
         for (int i = 0; i < LightsOut.GRID_SIZE; i++) {
             int x = LightsOut.TILE_MARGIN / 2;
             for (int j = 0; j < LightsOut.GRID_SIZE; j++) {
-                if (LightsOut.HAS_THE_WORLD_ENDED_YET) {
+                if (lightsClicked[i][j]) {
 
-                    // drawing the actuall tiles
-                    g2.setColor(Color.WHITE);
-                    // g2.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER,
-                    // ACTUALL_TILE_SIZE, ACTUALL_TILE_SIZE);
-                    g2.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
-                            LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
-                    // drawing the border of the tiles
-                } else {
                     // drawing the border
                     g2.setColor(Color.GREEN);
-                    g2.fillRect(x, y, LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN,
-                            LightsOut.TILE_SIZE - LightsOut.TILE_MARGIN);
+                    g2.fillRect(x, y, TILE_SIZE_WITH_MARGIN, TILE_SIZE_WITH_MARGIN);
                     // drawing the actuall tile
-                    g2.setColor(Color.WHITE);
+                    if (lights[i][j])
+                        g2.setColor(Color.WHITE);
+                    else
+                        g2.setColor(Color.BLACK);
                     g2.fillRect(x + LightsOut.TILE_BORDER, y + LightsOut.TILE_BORDER, TILE_SIZE_WITH_BORDER,
                             TILE_SIZE_WITH_BORDER);
 
+                } else {
+                    // drawing the actuall tiles
+                    if (lights[i][j])
+                        g2.setColor(Color.WHITE);
+                    else
+                        g2.setColor(Color.BLACK);
+                    g2.fillRect(x, y, TILE_SIZE_WITH_MARGIN, TILE_SIZE_WITH_MARGIN);
+                    // drawing the border of the tiles
                 }
-
                 x += LightsOut.TILE_SIZE;
             }
             y += LightsOut.TILE_SIZE;
