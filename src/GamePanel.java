fc7ea97fd84e55e94fe473dd56dfae18d73e8d96
Ynng <44907675+Ynng@users.noreply.github.com>
2018-12-25 19:18:37 -0500

    GamePanel now works with and adjust to rezising windows

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,9 +4,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel implements MouseListener {
-    static int borderedTileSize = LightsOut.tileSize - LightsOut.tileMargin - LightsOut.tileBorder * 2;
-    static int normalTileSize = LightsOut.tileSize - LightsOut.tileMargin;
-    static int gameDimension = LightsOut.tileSize * LightsOut.gridSize;
 
     // 2d arrays for storing the light states
     public boolean[][] lights;
@@ -18,6 +15,11 @@ public class GamePanel extends JPanel implements MouseListener {
     static int randomRange(int min, int max) {
         int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
+    }
+
+    public static int smaller(int a, int b){
+        if (a>b) return b;
+        else return a;
     }
 
     public static void printBools(boolean[][] input) {
@@ -33,7 +35,7 @@ public class GamePanel extends JPanel implements MouseListener {
         int count = 0;
         for (boolean[] list : input) {
             for (boolean item : list) {
-                if (item) {
+                    if (item) {
                     count++;
                 }
             }
@@ -63,8 +65,10 @@ public class GamePanel extends JPanel implements MouseListener {
 
     }
 
+    
+
     public GamePanel() {
-        setPreferredSize(new Dimension(gameDimension, gameDimension));
+        setPreferredSize(new Dimension(LightsOut.gridSize*LightsOut.tileSize, LightsOut.gridSize*LightsOut.tileSize));
         setBackground(Color.PINK);
 
         // initialize the two arrays
@@ -113,22 +117,26 @@ public class GamePanel extends JPanel implements MouseListener {
 
     @Override
     protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g);
-        int y = LightsOut.tileMargin / 2;
+        Graphics2D g2 = (Graphics2D) g;
+
+        int panelSize = smaller(getWidth(),getHeight());
+        int tileSize = panelSize/LightsOut.gridSize;
+
+        int y = (getHeight()-panelSize)/2 + LightsOut.tileMargin / 2;
         for (int row = 0; row < LightsOut.gridSize; row++) {
-            int x = LightsOut.tileMargin / 2;
+            int x = (getWidth()-panelSize)/2 + LightsOut.tileMargin / 2;
             for (int col = 0; col < LightsOut.gridSize; col++) {
                 if (lightsClicked[row][col] && LightsOut.debugMode) {
                     // drawing the border
                     g2.setColor(Color.GREEN);
-                    g2.fillRect(x, y, normalTileSize, normalTileSize);
+                    g2.fillRect(x, y, tileSize - LightsOut.tileMargin,tileSize - LightsOut.tileMargin);
                     // drawing the actuall tile
                     if (lights[row][col])
                         g2.setColor(Color.WHITE);
                     else
                         g2.setColor(Color.BLACK);
-                    g2.fillRect(x + LightsOut.tileBorder, y + LightsOut.tileBorder, borderedTileSize, borderedTileSize);
+                    g2.fillRect(x + LightsOut.tileBorder, y + LightsOut.tileBorder, tileSize - LightsOut.tileMargin - LightsOut.tileBorder * 2,tileSize - LightsOut.tileMargin - LightsOut.tileBorder * 2);
 
                 } else {
                     // drawing the actuall tiles
@@ -136,12 +144,12 @@ public class GamePanel extends JPanel implements MouseListener {
                         g2.setColor(Color.WHITE);
                     else
                         g2.setColor(Color.BLACK);
-                    g2.fillRect(x, y, normalTileSize, normalTileSize);
+                    g2.fillRect(x, y, tileSize - LightsOut.tileMargin,tileSize - LightsOut.tileMargin);
                     // drawing the border of the tiles
                 }
-                x += LightsOut.tileSize;
+                x += tileSize;
             }
-            y += LightsOut.tileSize;
+            y += tileSize;
         }
     }
 
@@ -150,18 +158,30 @@ public class GamePanel extends JPanel implements MouseListener {
         int mouseX = e.getX();
         int mouseY = e.getY();
         // System.out.println("Mouse X: " + mouseX + " Mouse Y: " + mouseY);
+        int panelSize = smaller(getWidth(),getHeight());
+        int tileSize = panelSize/LightsOut.gridSize;
+        //filter out invalid clicks
+        if(mouseX<((getWidth()-panelSize)/2)||mouseX>((getWidth()-panelSize)/2+panelSize)||mouseY<((getHeight()-panelSize)/2)||mouseY>((getHeight()-panelSize)/2+panelSize)){
+            return;
+        }
+        mouseX -= (getWidth()-panelSize)/2;
+        mouseY -= (getHeight()-panelSize)/2;
 
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
+        int col = mouseX / tileSize;
+        int row = mouseY / tileSize;
 
-        int boxWidth = panelWidth / lights[0].length;
-        int boxHeight = panelHeight / lights.length;
-
-        int col = mouseX / boxWidth;
-        int row = mouseY / boxHeight;
+        if(col>LightsOut.gridSize-1){
+            col=LightsOut.gridSize-1;
+            System.out.println("col out of bound");
+        }
+        if(row>LightsOut.gridSize-1){
+            row=LightsOut.gridSize-1;
+            System.out.println("row out of bound");
+        }
 
         stepCounter++;
         toggle(lights, row, col);
+
         lightsClicked[row][col] = !lightsClicked[row][col];
 
         if (0 == countBools(lights)) {
